f235af38317f08ceefe31bd89b47b4890daf1641
Ebbe Sylvester Svedborg Møller <ebbe@web-infusion.dk>
2016-06-08 11:47:40 +0200

    Removed settings

--- config/settings.php
+++ config/settings.php
@@ -4,15 +4,16 @@ define('_DEBUG_', false);
 
 define('_SQL_SERVER_', 'localhost');
 define('_SQL_PORT_', '3306');
-define('_SQL_DB_', 'fonograf');
-define('_SQL_USER_', 'fonograf');
-define('_SQL_PASS_', 'JGqQFu2MDF4W6ZKg');
+define('_SQL_DB_', '');
+define('_SQL_USER_', '');
+define('_SQL_PASS_', '');
 
 define('_PLAYER_SERVER_', 'localhost');
 define('_PLAYER_PORT_', '40000');
 define('_PLAYER_SECRET_', 'dbacb7e96b1d1d1d8542b389e836e7ccee3a66b0');
 
-define('_MUSIC_FOLDER_', '/mnt/usb/music');
+define('_MUSIC_FOLDER_', '');
 define('_MUSIC_TYPES_', json_encode([
   'mp3',
 ]));
+define('_LASTFM_KEY_', '');

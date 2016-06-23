70e0b458af5812fa289eee49e99e2d02b8de228a
fedtemis <ebbe@web-infusion.dk>
2016-06-23 23:03:45 +0000

    clean settings

---
+++ config/settings.php
@@ -0,0 +1,28 @@
+<?php
+define('_LANGUAGE_ISO_', 'da');
+define('_DEBUG_', false);
+
+define('_SQL_SERVER_', 'localhost');
+define('_SQL_PORT_', '3306');
+define('_SQL_DB_', '');
+define('_SQL_USER_', '');
+define('_SQL_PASS_', '');
+
+define('_PLAYER_SERVER_', 'localhost');
+define('_PLAYER_PORT_', '40000');
+define('_PLAYER_SECRET_', '');
+
+define('_COLOR_SERVER_', 'localhost');
+define('_COLOR_PORT_', '40001');
+define('_COLOR_SECRET_', '');
+
+define('_ADMIN_USER_', '');
+define('_ADMIN_PASS_', '');
+define('_MAGIC_WORD_', 'fonograf');
+
+define('_MUSIC_FOLDER_', '');
+define('_MUSIC_TYPES_', json_encode([
+  'mp3',
+]));
+
+define('_LASTFM_KEY_', '');
